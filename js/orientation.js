2294c74fc2bdaf9e121c1feb5ace8a6ec33deed9
kofway <kofway@hotmail.com>
2012-06-21 10:44:32 -0700

    init

---
+++ js/orientation.js
@@ -0,0 +1,13 @@
+var v = $("#phone"); var scale, angle;
+var classSet = false;
+window.addEventListener("deviceorientation", function(e) {
+if (!classSet) {
+	document.body.classList.add("orientation");
+	classSet = true;
+}
+angle = e.gamma + "deg"; scale = (e.beta + 30) / 60;
+}, true);
+
+window.setInterval(function() {
+	v.style.MozTransform = "rotate(" + angle + ") scale(" + scale + ")";
+}, 100);
