de3e7ac955617ea88d633eb036d98d3aef24cbb5
kofway <kofway@hotmail.com>
2012-07-17 16:11:24 -0700

    orientation test

--- js/orientation.js
+++ js/orientation.js
@@ -1,16 +1,18 @@
 (function () {
-var v = $("#phone"); var scale, angle;
-var classSet = false;
-window.addEventListener("deviceorientation", function(e) {
-if (!classSet) {
-	document.body.classList.add("orientation");
-	classSet = true;
-}
-angle = e.gamma + "deg"; scale = (e.beta + 30) / 60;
-}, true);
-
-window.setInterval(function() {
-	v.style.MozTransform = "rotate(" + angle + ") scale(" + scale + ")";
-}, 100);
+alert("Screen orientation state is " + screen.orientation);
+	var v = $("#phone"); 
+	var scale, angle;
+	var classSet = false;
+	window.addEventListener("deviceorientation", function(e) {
+	if (!classSet) {
+		document.body.classList.add("orientation");
+		classSet = true;
+	}
+	angle = e.gamma + "deg"; scale = (e.beta + 30) / 60;
+	}, true);
+	
+	window.setInterval(function() {
+		v.style.MozTransform = "rotate(" + angle + ") scale(" + scale + ")";
+	}, 100);
 
 })();
