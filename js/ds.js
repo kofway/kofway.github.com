9eb9610f50b83da781812dc4991adf4b7bca57e4
kofway <kofway@hotmail.com>
2012-07-19 14:14:51 -0700

    second change, lots of commeted

--- js/ds.js
+++ js/ds.js
@@ -1,18 +1,48 @@
 (function () {
 	var dsStatus = document.getElementById("ds_status");
 	var dsCheck = document.getElementById("ds_check");
-	var ds = navigator.getDeviceStorage("pictures");
+	var ds = navigator.getDeviceStorage("profile");
+/*	ok(navigator.getDeviceStorage, "Should have getDeviceStorage");
+	
+	request = storage[0].addNamed(createRandomBlob(), "devicestorage/hi");
+	ok(request, "Should have a non-null request");
+	
+	request.onsuccess = addSuccess;
+	request.onerror = addError;
+	
+	function addSuccess(e) {
+		    dsStatus.innerHTML = "PASS";
+
+		ok(e.target.result == gFileName, "File name should match");
+		var storage = navigator.getDeviceStorage("profile");
+		request = storage[0].get(gFileName);
+		request.onsuccess = getSuccess;
+		request.onerror = getError;
+		ok(true, "addSuccess was called");
+	}
+
+
+function addError(e) {
+    dsStatus.innerHTML = "FAIL";
+
+ok(false, "addError was called : " + e.target.error.name);
+devicestorage_cleanup();
+
+}
+*/
+
+
 	
 	
-	function dstest() { 
-		var request = ds[0].addNamed(googletest, "https://www.google.com/images/srpr/logo3w.png");
-	    if (false){
+	/*function dstest() { 
+		//var request = ds[0].addNamed(googletest, "https://www.google.com/images/srpr/logo3w.png");
+	    if (ds){
 	        dsStatus.innerHTML = "PASS";
 	    }else{
 		    dsStatus.innerHTML = "FAIL";
 	    }
     }
     
-    dstest();
+    dstest();*/
     dsStatus.innerHTML = "FAIL";
 })();
