21625e6a9fca618253cc09047187029c96ebde1b
kofway <kofway@hotmail.com>
2012-07-19 14:00:48 -0700

    test for devicestorage api

--- js/ds.js
+++ js/ds.js
@@ -1,18 +1,18 @@
 (function () {
 	var dsStatus = document.getElementById("ds_status");
 	var dsCheck = document.getElementById("ds_check");
-	//var ds = navigator.getDeviceStorage("pictures");
+	var ds = navigator.getDeviceStorage("pictures");
 	
 	
-	/*function dstest() { 
-		//var request = ds[0].addNamed(googletest, "https://www.google.com/images/srpr/logo3w.png");
+	function dstest() { 
+		var request = ds[0].addNamed(googletest, "https://www.google.com/images/srpr/logo3w.png");
 	    if (false){
 	        dsStatus.innerHTML = "PASS";
 	    }else{
 		    dsStatus.innerHTML = "FAIL";
 	    }
-    }*/
+    }
     
-    //dstest();
+    dstest();
     dsStatus.innerHTML = "FAIL";
 })();
