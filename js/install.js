0c6200686fdf6bdc78b90c3d7bd8013f78738ae7
kofway <kofway@hotmail.com>
2012-08-08 16:34:59 -0700

    commit install

--- js/install.js
+++ js/install.js
@@ -4,9 +4,11 @@
 		var request = navigator.mozApps.install("http://kofway.github.com/manifest.webapp");  
 		var installStatus = document.getElementById("install_status");
 	    request.onsuccess = function() {  
+	    	console.log("install starting");
 		    installStatus.innerHTML = "PASS";
 	    }  
 	    request.onerror = function() {  
+	    	console.log("install error");
 	     	installStatus.innerHTML = "FAIL";
 	    }      
     }, false);
