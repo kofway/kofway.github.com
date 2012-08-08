9b9d23e6dc4a0eb5ff5deb1ae2884c7d1bcccbc5
kofway <kofway@hotmail.com>
2012-08-08 16:09:29 -0700

    test button verify

--- js/install.js
+++ js/install.js
@@ -1,7 +1,14 @@
 (function () {
     document.querySelector("#install_button").addEventListener("click", function () {
     	console.log("install click");
-
+		var request = navigator.mozApps.install("http://kofway.github.com/manifest.webapp");  
+		var installStatus = document.getElementById("install_status"),
+	    request.onsuccess = function() {  
+		    installStatus.innerHTML = "PASS";
+	    }  
+	    request.onerror = function() {  
+	     	installStatus.innerHTML = "FAIL";
+	    }      
     }, false);
 
 })();
