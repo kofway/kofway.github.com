3c846092c816f88550381b3cdb86430ace03e7dd
kofway <kofway@hotmail.com>
2012-08-08 15:58:37 -0700

    install

--- js/install.js
+++ js/install.js
@@ -1,6 +1,6 @@
 (function () {
     document.querySelector("#install_button").addEventListener("click", function () {
-		var request = navigator.mozApps.install("http://kofway.github.com/");  
+		var request = navigator.mozApps.install("http://kofway.github.com/manifest.webapp");  
 		var installStatus = document.getElementById("install_status"),
 	    request.onsuccess = function() {  
 		    installStatus.innerHTML = "PASS";
