30b8cfb2b2949bf0fc04c23288cce4c66331bc72
kofway <kofway@hotmail.com>
2012-08-08 17:55:13 -0700

    changed debug

--- js/start.js
+++ js/start.js
@@ -3,7 +3,7 @@
 	request.onsuccess = function() {  
   if (request.result) {  
     // we're installed  
-    alert('This app is installed.');
+    // alert('This app is installed.');
   } else {  
     // not installed  
     alert('This app is not currently installed.');
