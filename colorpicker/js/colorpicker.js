3366d28dec39634fa1ffdac22b9c0912296aa376
kofway <kofway@hotmail.com>
2014-05-15 15:07:32 -0400

    more backend changes

--- colorpicker/js/colorpicker.js
+++ colorpicker/js/colorpicker.js
@@ -1,44 +1,49 @@
-	// hash table for color properties
-	var cmap = new Object();
-	cmap.c11c = "葡萄鼠";
-	cmap.c11j = "ぶどうねずみ";
-	cmap.c11e = "Grape<br>Rat";
-	cmap.c11o = "#705b67";
-	cmap.c11f = "0";
-	cmap.c12c = "薄桜";
-	cmap.c12j = "うすざくら";
-	cmap.c12e = "Light<br>Sakura";
-	cmap.c12o = "#fdeff2";
-	cmap.c12f = "1";
-	cmap.c13c = "萌黄";
-	cmap.c13j = "もえぎ";
-	cmap.c13e = "Moe<br>Yellow";
-	cmap.c13o = "#aacf53";
-	cmap.c13f = "1";
-
 	// function to assign color properties to page elements
-	function assignColor(divname){
-		// assign page fields to var
+	function assignColor(divname, colorName){
+		// assign page elements to var
 		var cname = document.getElementById(divname.id+'c');
 		var jname = document.getElementById(divname.id+'j');
 		var ename = document.getElementById(divname.id+'e');
-		var colorCode = cmap[divname.id+'o'];
+		var colorCode = cmap[colorName].o;
 		
-		// assign color properties to page fields
-		cname.innerHTML = cmap[cname.id];
-		jname.innerHTML = cmap[jname.id];
-		ename.innerHTML = cmap[ename.id];
+		// assign color properties to page elements
+		cname.innerHTML = cmap[colorName].c;
+		jname.innerHTML = cmap[colorName].j;
+		ename.innerHTML = cmap[colorName].e;
 		divname.style.backgroundColor = colorCode;
-		if (cmap[divname.id+'f'] != 0){
+		if (cmap[colorName.f] != 0){
 			divname.style.color = "#000000";
 		} else {
 			divname.style.color = "#FFFFFF";
 		}
-		
 	}
+
+	// hash table for color properties
+	var cmap = new Object();
+		cmap.grapeRat = new Object();
+		cmap.grapeRat.c = "葡萄鼠";
+		cmap.grapeRat.j = "ぶどうねずみ";
+		cmap.grapeRat.e = "Grape<br>Rat";
+		cmap.grapeRat.o = "#705b67";
+		cmap.grapeRat.f = "0";
+		
+		cmap.lightSakura = new Object();
+		cmap.lightSakura.c = "薄桜";
+		cmap.lightSakura.j = "うすざくら";
+		cmap.lightSakura.e = "Light<br>Sakura";
+		cmap.lightSakura.o = "#fdeff2";
+		cmap.lightSakura.f = "1";	
+
+		cmap.moeYellow = new Object();
+		cmap.moeYellow.c = "萌黄";
+		cmap.moeYellow.j = "もえぎ";
+		cmap.moeYellow.e = "Moe<br>Yellow";
+		cmap.moeYellow.o = "#aacf53";
+		cmap.moeYellow.f = "1";	
+
 	
-	assignColor(c11);
-	assignColor(c12);
-	assignColor(c13);
+	assignColor(c11, "grapeRat");
+	assignColor(c12, "lightSakura");
+	assignColor(c13, "moeYellow");
 
 
