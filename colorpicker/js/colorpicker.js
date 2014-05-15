1d56366a36ae6e5c540336fcc893a79b997f822f
kofway <kofway@hotmail.com>
2014-05-15 14:17:46 -0400

    layout

--- colorpicker/js/colorpicker.js
+++ colorpicker/js/colorpicker.js
@@ -2,17 +2,17 @@
 	var cmap = new Object();
 	cmap.c11c = "葡萄鼠";
 	cmap.c11j = "ぶどうねずみ";
-	cmap.c11e = "Grape Rat";
+	cmap.c11e = "Grape<br>Rat";
 	cmap.c11o = "#705b67";
 	cmap.c11f = "0";
 	cmap.c12c = "薄桜";
 	cmap.c12j = "うすざくら";
-	cmap.c12e = "Light Sakura";
+	cmap.c12e = "Light<br>Sakura";
 	cmap.c12o = "#fdeff2";
 	cmap.c12f = "1";
 	cmap.c13c = "萌黄";
 	cmap.c13j = "もえぎ";
-	cmap.c13e = "Moe Yellow";
+	cmap.c13e = "Moe<br>Yellow";
 	cmap.c13o = "#aacf53";
 	cmap.c13f = "1";
 
