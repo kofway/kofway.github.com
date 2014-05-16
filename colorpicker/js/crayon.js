c2c328f4afac23e812c36e18adebdc5285f008e7
kofway <kofway@hotmail.com>
2014-05-16 15:42:19 -0400

    more crayon

--- colorpicker/js/crayon.js
+++ colorpicker/js/crayon.js
@@ -18,36 +18,73 @@
 			divname.style.color = "#FFFFFF";
 		}
 		link.title = colorCode.toUpperCase() + ": " + cname.innerHTML +"色";
-		link.href = "img/" + colorName + ".jpg";
+		link.href = "img/crayon/" + colorName + ".jpg";
 	}
 
 	// hash table for color properties
 	var cmap = new Object();
-		cmap.grapeRat = new Object();
-		cmap.grapeRat.c = "葡萄鼠";
-		cmap.grapeRat.j = "ぶどうねずみ";
-		cmap.grapeRat.e = "Grape<br>Rat";
-		cmap.grapeRat.o = "#705b67";
-		cmap.grapeRat.f = "w";
+	/* Template
+		cmap.colorName = new Object();
+		cmap.colorName.c = "白";
+		cmap.colorName.j = "白";
+		cmap.colorName.e = "White";
+		cmap.colorName.o = "#000000";
+		cmap.colorName.f = "b";
+	*/
+		cmap.skyBlue = new Object();
+		cmap.skyBlue.c = "天空蓝";
+		cmap.skyBlue.j = "空色";
+		cmap.skyBlue.e = "Sky<br>Blue";
+		cmap.skyBlue.o = "#80DAEB";
+		cmap.skyBlue.f = "b";
 		
-		cmap.lightSakura = new Object();
-		cmap.lightSakura.c = "薄桜";
-		cmap.lightSakura.j = "うすざくら";
-		cmap.lightSakura.e = "Light<br>Sakura";
-		cmap.lightSakura.o = "#fdeff2";
-		cmap.lightSakura.f = "b";	
+		cmap.yellowOrange = new Object();
+		cmap.yellowOrange.c = "橙黄";
+		cmap.yellowOrange.j = "橙黄";
+		cmap.yellowOrange.e = "Yellow<br>Orange";
+		cmap.yellowOrange.o = "#FFAE42";
+		cmap.yellowOrange.f = "w";	
 
-		cmap.moeYellow = new Object();
-		cmap.moeYellow.c = "萌黄";
-		cmap.moeYellow.j = "もえぎ";
-		cmap.moeYellow.e = "Moe<br>Yellow";
-		cmap.moeYellow.o = "#aacf53";
-		cmap.moeYellow.f = "b";	
+		cmap.yellowGreen = new Object();
+		cmap.yellowGreen.c = "黄绿";
+		cmap.yellowGreen.j = "黄緑";
+		cmap.yellowGreen.e = "Yellow<br>Green";
+		cmap.yellowGreen.o = "#C5E384";
+		cmap.yellowGreen.f = "b";	
+		
+		cmap.caribbeanGreen = new Object();
+		cmap.caribbeanGreen.c = "加勒比绿";
+		cmap.caribbeanGreen.j = "カリビアン緑";
+		cmap.caribbeanGreen.e = "Caribbean<br>Green";
+		cmap.caribbeanGreen.o = "#00CC99";
+		cmap.caribbeanGreen.f = "b";
+		
+		cmap.blizzardBlue = new Object();
+		cmap.blizzardBlue.c = "暴雪蓝";
+		cmap.blizzardBlue.j = "暴風雪青";
+		cmap.blizzardBlue.e = "Blizzard<br>Blue";
+		cmap.blizzardBlue.o = "#ACE5EE";
+		cmap.blizzardBlue.f = "b";
+		
+		cmap.violetRed = new Object();
+		cmap.violetRed.c = "紫红";
+		cmap.violetRed.j = "紫赤";
+		cmap.violetRed.e = "Violet<br>Red";
+		cmap.violetRed.o = "#F75394";
+		cmap.violetRed.f = "b";
+		
+		cmap.sunGlow = new Object();
+		cmap.sunGlow.c = "阳光";
+		cmap.sunGlow.j = "太陽の輝";
+		cmap.sunGlow.e = "Sun<br>Glow";
+		cmap.sunGlow.o = "#FFCF48";
+		cmap.sunGlow.f = "b";
 
 	
-	assignColor(c1, "grapeRat");
-	assignColor(c2, "lightSakura");
-	assignColor(c3, "moeYellow");
-
-
-
+	assignColor(c1, "skyBlue");
+	assignColor(c2, "yellowOrange");
+	assignColor(c3, "yellowGreen");
+	assignColor(c4, "caribbeanGreen");
+	assignColor(c5, "blizzardBlue");
+	assignColor(c6, "violetRed");
+	assignColor(c7, "sunGlow");
