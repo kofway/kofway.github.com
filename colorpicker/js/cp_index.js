110747829c4507a3601d93c5accbc41f29012a91
kofway <kofway@hotmail.com>
2014-05-16 14:41:14 -0400

    crayon

--- colorpicker/js/cp_index.js
+++ colorpicker/js/cp_index.js
@@ -1,4 +1,4 @@
-	// function to assign color properties to page elements
+	// function to assign catagory properties to page elements
 	function assignColor(divname, colorName){
 		// assign page elements to var
 		var cname = document.getElementById(divname.id+'c');
@@ -7,47 +7,32 @@
 		var link = document.getElementById(divname.id+'a');
 		var colorCode = cmap[colorName].o;
 		
-		// assign color properties to page elements
+		// assign catagory properties to page elements
 		cname.innerHTML = cmap[colorName].c;
 		jname.innerHTML = cmap[colorName].j;
 		ename.innerHTML = cmap[colorName].e;
 		divname.style.backgroundColor = colorCode;
 		if (cmap[colorName].f == 'b'){
-			divname.style.color = "#FFFFFF";
-		} else {
 			divname.style.color = "#000000";
+		} else {
+			divname.style.color = "#FFFFFF";
 		}
-		link.title = colorCode.toUpperCase() + ": " + cname.innerHTML +"色";
-		link.href = "img/" + colorName + ".jpg";
+		//link.title = colorCode.toUpperCase() + ": " + cname.innerHTML +"色";
+		//link.href = "img/" + colorName + ".jpg";
 	}
 
-	// hash table for color properties
+	// hash table for catagory properties
 	var cmap = new Object();
-		cmap.grapeRat = new Object();
-		cmap.grapeRat.c = "葡萄鼠";
-		cmap.grapeRat.j = "ぶどうねずみ";
-		cmap.grapeRat.e = "Grape<br>Rat";
-		cmap.grapeRat.o = "#705b67";
-		cmap.grapeRat.f = "b";
-		
-		cmap.lightSakura = new Object();
-		cmap.lightSakura.c = "薄桜";
-		cmap.lightSakura.j = "うすざくら";
-		cmap.lightSakura.e = "Light<br>Sakura";
-		cmap.lightSakura.o = "#fdeff2";
-		cmap.lightSakura.f = "w";	
+		cmap.crayon = new Object();
+		cmap.crayon.c = "蜡笔";
+		cmap.crayon.j = "クレヨン";
+		cmap.crayon.e = "Crayon<br>Colors";
+		cmap.crayon.o = "#FFFFFF";
+		cmap.crayon.f = "b";
 
-		cmap.moeYellow = new Object();
-		cmap.moeYellow.c = "萌黄";
-		cmap.moeYellow.j = "もえぎ";
-		cmap.moeYellow.e = "Moe<br>Yellow";
-		cmap.moeYellow.o = "#aacf53";
-		cmap.moeYellow.f = "w";	
 
 	
-	assignColor(c1, "grapeRat");
-	assignColor(c2, "lightSakura");
-	assignColor(c3, "moeYellow");
+	assignColor(c1, "crayon");
 
 
 
