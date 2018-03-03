483090e28ef2f5f5bd38f672052c85f54d65cd91
xwei <kofway@hotmail.com>
2018-03-02 19:20:05 -0500

    remove old pages

--- archive/kanai_cube/scripts/_references.js
+++
@@ -1,6 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="jquery-1.11.3.min.js" />
-/// <reference path="bootstrap/js/bootstrap.js" />
-/// <reference path="knockout-3.3.0.js" />
-/// <reference path="knockout.persist.js" />
-/// <reference path="knockout.mapping-latest.js" />
